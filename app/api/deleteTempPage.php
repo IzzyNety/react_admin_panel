48721457530359c2a41ecc2f6000c06b6355745a
IzzyNety <izzy.nety@gmail.com>
2020-08-20 16:01:08 +0500

    Реализация открытия других страниц в проекте

--- app/api/deleteTempPage.php
+++ app/api/deleteTempPage.php
@@ -1,6 +1,5 @@
 <?php
-$_POST = json_decode(file_get_contents("php://input"), true);
-$file = "../../" . $_POST["name"];
+$file = "../../hjasdifhi23__rejah2.html";
 
 if (file_exists($file)) {
     unlink($file);
