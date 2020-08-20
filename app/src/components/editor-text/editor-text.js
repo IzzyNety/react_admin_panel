5fdf21821fd6156d6e3b9507fdcd8bd046d4f481
IzzyNety <izzy.nety@gmail.com>
2020-08-20 12:35:29 +0500

    включаем редактироваие ссылок

--- app/src/components/editor-text/editor-text.js
+++ app/src/components/editor-text/editor-text.js
@@ -6,6 +6,14 @@ export default class EditorText {
     this.element.addEventListener("blur", () => this.onBlur());
     this.element.addEventListener("keypress", (e) => this.onKeypress(e));
     this.element.addEventListener("input", () => this.onTextEdit());
+    if(this.element.parentNode.nodeName === "A" || this.element.parentNode.nodeName === "BUTTOM") {
+      this.element.addEventListener("contextmenu", (e) => this.onCtxMenu(e));
+    }
+  }
+
+  onCtxMenu(e) {
+    e.preventDefault();
+    this.onClick();
   }
 
   onKeypress(e) {
